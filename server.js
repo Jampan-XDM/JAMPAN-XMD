bf4910a04ce2b671eec19c5793406a58afea4282
Jampan-XDM <jampankelvin@gmail.com>
2026-05-05 19:10:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { startBot, getStatus, requestPair } from "./bot.js";
 
-const app = express();
-app.use(express.json());
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
 let currentStatus = "offline";
 
 // Start bot system
